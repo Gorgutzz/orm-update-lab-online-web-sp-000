2e4fc845d7f442dd07f320b58d08d1df81c517ec
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-05-23 01:17:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,9 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id 
+
   def initiliaze(id=nil, name, grade)
     @name = name
     @grade = grade
