76b3cef83ca8a05103494bc857bd3d23b6a104c8
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-05-23 01:13:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,12 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  def initiliaze(id = nil, name, grade)
+  def initiliaze(id=nil, name, grade)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
 
 
   # Remember, you can access your database connection anywhere in this class
